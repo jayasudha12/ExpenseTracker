cf2a506b58b7cef84adeb6a0c2dc87c5343d22fc
JAYASUDHA T <jayasudha.t2022cse@sece.ac.in>
2024-03-13 11:44:23 +0530

    Basic Setup

--- Schema.js
+++ Schema.js
@@ -1,20 +1,44 @@
 // we use mangoose for defining schema
-const mongoose = require('mongoose')
-const expensetrackerschema = new mongoose.Schema({
-    Amount : {
-        type : Number
-    },
-    Category : {
-        type : String
-    },
-    Date : {
-        type : String
-    }
-})
-// we have to create the model to access the data like fetch .It should be in capital letter
 /**
- * 1 st argument , 2 nd argument -> collection name, schema name
+ * to create the new schema we have to create the model and export it using the syntax
+ *                    syntax : module.exports =  {expense,user}
+ * we 
  */
-const Expense = mongoose.model('expensedetails',expensetrackerschema)
-// exporting..
+// const mongoose = require('mongoose')
+// const expensetrackerschema = new mongoose.Schema({
+//     Amount : {
+//         type : Number
+//         // required : true
+//     },
+//     Category : {
+//         type : String
+//     },
+//     Date : {
+//         type : String
+//     }
+// })
+// // we have to create the model to access the data like fetch .It should be in capital letter
+// /**
+//  * 1 st argument , 2 nd argument -> collection name, schema name
+//  */
+// const Expense = mongoose.model('expensedetails',expensetrackerschema)
+// // exporting..
+// module.exports = {Expense}
+const mongoose = require('mongoose')
+const expenseTrackerSchema =  mongoose.Schema({
+      amount : {
+        type : Number
+        //required : true
+      },
+      category : {
+        type : String
+      },
+      date : {
+        type : String
+      }
+})
+
+const Expense = mongoose.model('expensedetails',expenseTrackerSchema) //1st parameter - collection name 2nd parameter - schema name
+
+//Exporting
 module.exports = {Expense}
--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const {Expense}= require('./Schema.js')
 const app = express1()
 app.use(bodyparser.json())
 app.use(cors())
+/**
+ * cors ->
+ * it is a open source network -> we can access it from anywhere
+ * it will give all the response to the origin from different area
+ * request sent -> check origin -> but cors will be accessing
+ * Middleware ->
+ * predefined middleware -> body-parser,cors-parser
+ * post -> it will be used to handle just before the middleware
+ * request handle ->[ middleware ]-> response
+ */
+/**
+ * Intermediate between the client side and database
+ */
+
 /**Expense tracker functionality
  * adding a new expense/income :/add_expense ->post
  * editing existing entries :/edit_expense -> put/patch
@@ -46,105 +60,172 @@ catch(error){
 }
 }
 connectToDB()
-app.post('/add-expense',async function(request,response){
+// app.post('/add-expense',async function(request,response){
+//     // console.log(request.body)
+//     // response.status(200).json({
+//     //     "Status" : "Inserting the data...."
+//     // })
+//     try{
+//     await Expense.create({
+//         "Amount" : request.body.Amount,
+//         "Category" :request.body.Category ,
+//         "Date" : request.body.Date
+//     })//it is asynchronous
+//     response.status(201).json({
+//         "status" : "successfully inserted into db",
+//         "Message" : "New Entry created"
+//     })
+// }
+// catch(error){
+//     response.status(500).json({
+//      'message' :'Error occurred could not  be inserted',
+//      'status'  : 'failure',
+//      'error'   : error
+// })
+// }
+// })
+// app.get('/get-expense',async function(request,response){
+//     try{
+//     const expenses = await Expense.find()
+//     response.status(200).json({expenses})
+//     }
+//     catch(error){
+//         response.status(500).json({
+//         'message' :'Error occurred could not  be displayed',
+//         'status'  : 'failure',
+//         'error'   : error
+//          })
+//     }
+// })
+// // params -> url ,  "_id": "65e69a6247e26a67dd7e4d4c"
+// app.delete('/delete-expense/:id',async function(request,response){
+    
+    
+//    // console.log(data)
+//    try{
+//     const data = await Expense.findById(request.params.id)
+//    if(data){
+//         await Expense.findByIdAndDelete(request.params.id)
+//         response.status(200).json({
+//             "Status" : "successfully deleted :)",
+//             "Message" : "entry deleted"
+//         })
+//        }
+//        else{
+//         response.status(404).json({
+//             "Status" : "failure",
+//             "Message" : "file not found!!!"
+//         })
+//        }
+    
+// }
+// catch(error){
+//     response.status(500).json({
+//     'message' :'Internal Server Error',
+//     'status'  : 'failure',
+//     'error'   : error
+//      })
+// }
+// })
+// app.patch('/edit-expense/:id',async function(request,response){
+//     try{
+//         const data1 = await Expense.findById(request.params.id)
+//    if(data1){
+//         await data1.updateOne({
+//             "Amount" : request.body.Amount,
+//             "Category" :request.body.Category ,
+//             "Date" : request.body.Date
+//         })
+//         response.status(200).json({
+//             "Status" : "successfully updated :)",
+//             "Message" : "updated!!!"
+//         })
+
+//        }
+//        else{
+//         response.status(404).json({
+//             "Status" : "failure",
+//             "Message" : "file not found!!!"
+//         })
+//        }
+//     }
+//    catch(error){
+//     response.status(500).json({
+//         'message' :'Internal Server Error could not be updated',
+//         'status'  : 'failure',
+//         'error'   : error
+//          })
+//    }
+// })
+
+
+
+// //console.log("Hello")
+// //_ _v = 0 it is like a version we acn also avoid that
+
+
+app.post('/addexpense',async function(request,response){
     // console.log(request.body)
-    // response.status(200).json({
-    //     "Status" : "Inserting the data...."
+    // response.json({
+    //     "Status" : "Inserting Data"
     // })
+    //Queries are async functions
     try{
-    await Expense.create({
-        "Amount" : request.body.Amount,
-        "Category" :request.body.Category ,
-        "Date" : request.body.Date
-    })//it is asynchronous
-    response.status(201).json({
-        "status" : "successfully inserted into db",
-        "Message" : "New Entry created"
-    })
-}
-catch(error){
-    response.status(500).json({
-     'message' :'Error occurred could not  be inserted',
-     'status'  : 'failure',
-     'error'   : error
-})
-}
-})
-app.get('/get-expense',async function(request,response){
-    try{
-    const expenses = await Expense.find()
-    response.status(200).json({expenses})
+        await Expense.create({
+            'amount' : request.body.amount,
+            'category' : request.body.category,
+            'date' : request.body.date
+        })
+        response.status(201).json({'status' : 'inserted successfully','message' : 'one entry created'})
     }
     catch(error){
-        response.status(500).json({
-        'message' :'Error occurred could not  be displayed',
-        'status'  : 'failure',
-        'error'   : error
-         })
+        response.status(500).json({'status' : 'Failure','message' : 'entry not created','error' : error})
     }
+    
 })
-// params -> url ,  "_id": "65e69a6247e26a67dd7e4d4c"
-app.delete('/delete-expense/:id',async function(request,response){
-    
-    
-   // console.log(data)
-   try{
-    const data = await Expense.findById(request.params.id)
-   if(data){
-        await Expense.findByIdAndDelete(request.params.id)
-        response.status(200).json({
-            "Status" : "successfully deleted :)",
-            "Message" : "entry deleted"
-        })
-       }
-       else{
-        response.status(404).json({
-            "Status" : "failure",
-            "Message" : "file not found!!!"
-        })
-       }
-    
-}
-catch(error){
-    response.status(500).json({
-    'message' :'Internal Server Error',
-    'status'  : 'failure',
-    'error'   : error
-     })
-}
-})
-app.patch('/edit-expense/:id',async function(request,response){
-    try{
-        const data1 = await Expense.findById(request.params.id)
-   if(data1){
-        await data1.updateOne({
-            "Amount" : request.body.Amount,
-            "Category" :request.body.Category ,
-            "Date" : request.body.Date
-        })
-        response.status(200).json({
-            "Status" : "successfully updated :)",
-            "Message" : "updated!!!"
-        })
 
-       }
-       else{
-        response.status(404).json({
-            "Status" : "failure",
-            "Message" : "file not found!!!"
-        })
-       }
-    }
+app.get('/getexpense',async function(request,response){
+   try{
+      const data = await Expense.find()
+       response.status(200).json(data)
+   }
    catch(error){
-    response.status(500).json({
-        'message' :'Internal Server Error could not be updated',
-        'status'  : 'failure',
-        'error'   : error
-         })
+       response.status(500).json({'status' : 'failure','message' : 'entry not found','error' : error})
    }
 })
 
+app.delete('/deleteexpense/:id',async function(request,response){
+    try{
+       const data = await Expense.findById(request.params.id)
+       if(data){
+        await Expense.findByIdAndDelete(request.params.id)
+        response.status(200).json({'status' : 'success','message' : 'entry deleted'})
+       }
+       else{
+        response.status(404).json({'status' : 'failure','message' : 'file not found'})
+       }
+    }
+    catch(error){
+        response.status(500).json({'status' : 'failure','message' : 'could not delete entry','error' : error})
+    }
+})
 
-
-//console.log("Hello")
-//_ _v = 0 it is like a version we acn also avoid that
+app.patch('/editexpense/:id',async function(request,response){
+    try{
+      const data = await Expense.findById(request.params.id)
+      if(data){
+        await data.updateOne({
+         'amount' : request.body.amount,
+         'category' : request.body.category,
+         'date' : request.body.date
+        })
+        response.status(404).json({'status' : 'success','message' : 'entry updated'})
+      }
+      else{
+        response.status(404).json({'status' : 'failure','message' : 'file not found'})
+      }
+    }
+    catch(error){
+        response.status(500).json({'status' : 'failure','message' : 'could not update entry','error' : error})
+    }
+})
